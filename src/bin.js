d6ec1910e1fd873a45111f61f4888ce24e4c0c8e
Florian Albertz <flo.albertz@gmail.com>
2015-09-12 13:27:24 +0200

    I am an idiot

---
+++ src/bin.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+'use strict';
+
+import app from 'commander';
+import pkg from '../package.json';
+import store from './tstore.js';
+
+app.version(pkg.version);
+
+app.command('get <key>')
+	.description('Print data directly to stdout')
+	.action(key => {
+		let data = store.get(key);
+
+		/* Log in single line if used to pipe stuff */
+		console.log(JSON.stringify(data, 0, process.stdout.isTTY ? 2 : 0));
+	});
+
+app.command('set <key> <json>')
+	.description('Set data from json string')
+	.action((key, json) => {
+		store.set(key, JSON.parse(json));
+	});
+
+app.parse(process.argv);
